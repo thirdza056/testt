deab500df255a53a79c5109d1598e768a52f5b79
nifira123 <bustami2000@gmail.com>
2017-06-05 10:43:21 +0700

    Add files via upload

---
+++ menu.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#Menu
+
+echo -e "============MENU=============="
+echo -e "usernew    : membuat akun SSH" 
+echo -e "login      : cek user login"
+echo -e "member     : cek member SSH"
+echo -e "speedtest  : speedtest VPS"
+echo -e "=============================="
