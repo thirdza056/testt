f80f74e91d517a99784763853337cef5af7ba937
nifira123 <bustami2000@gmail.com>
2017-06-11 11:31:54 +0700

    Delete hapus.sh

--- hapus.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Script hapus user SSH
-# Created by Abu Yazid Al-Busthami
-
-read -p "Nama user SSH yang akan dihapus : " Nama
-
-userdel -r $Nama
-echo -e "User SSH dengan nama $Nama telah sukses dihapus"
