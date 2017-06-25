3b3eba8c76f3099ec570defe9ecbe88931d0e5a2
Bustami Arifin <bustami2000@gmail.com>
2017-06-25 20:56:08 +0700

    Add files via upload

---
+++ about.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#About this script
+
+IP=`dig +short myip.opendns.com @resolver1.opendns.com`
+echo -e ""
+echo -e "===================================================="
+echo -e "Script Auto Install SSH & OpenVPN v1.0" 
+echo -e "===================================================="
+echo -e ""
+echo -e "For Debian 7 32 bit & 64 bit"
+echo -e "Only for VPS with KVM and VMWare virtualization"
+echo -e ""
+echo -e "Original script by :"
+echo -e "* Fornesia"
+echo -e "* Rzengineer"
+echo -e "* Fawzya"
+echo -e ""
+echo -e "Modified by Bustami Arifin a.k.a Abu Yazid Al-Busthami"
+echo -e ""
+echo -e "Contact Person : https://www.facebook.com/bustami.arifin.127"
+echo -e "Website        : http://www.masarif.tk"
+echo -e ""
+
