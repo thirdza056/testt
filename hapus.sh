90c734aa1a89a2a5c893ef9a388499836abf8be1
nexne <nexne@unpak.ac.id>
2017-07-01 16:37:09 +0800

    Update hapus.sh

--- hapus.sh
+++ hapus.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-# Script hapus user SSH
-# Created by Abu Yazid Al-Busthami
+#Script untuk menghapus user
 
-read -p "Nama user SSH yang akan dihapus : " Nama
+read -p "Hapus User : " Pengguna
 
-userdel -r $Nama
+if getent passwd $Pengguna > /dev/null 2>&1; then
+        userdel $Pengguna
+        echo -e "User $Pengguna telah dihapus."
+else
+        echo -e "GAGAL: User $Pengguna tidak ada."
+fi
